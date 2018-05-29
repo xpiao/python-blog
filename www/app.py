95f619d8b1da4d8c30dc31be12d1b899cb98a55b
xpiao <xmin_02@163.com>
2018-05-29 22:47:52 +0800

    #day2, add comment

--- www/app.py
+++ www/app.py
@@ -1,24 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import logging; logging.basicConfig(level=logging.INFO)
 
-import asyncio, os, json, time
-from datetime import datetime
+import asyncio
 
-from aiohttp import web
+# aiohttp基于asyncio的异步http框架。
+from aiohttp import web  # 用于实现web服务器
 
 
+# 不加requests的情况：TypeError: index() takes 0 positional arguments but 1 was given
 def index(requests):
     return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
 
+
+# 初始化
 @asyncio.coroutine
 def init(loop):
-    app = web.Application(loop=loop)
+    app = web.AppRunner(loop=loop)  # 创建app对象
     app.router.add_route('GET', '/', index)
+    # 调用子协程:创建一个TCP服务器,绑定到"127.0.0.1:9000"socket,并返回一个服务器对象
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
+
+loop = asyncio.get_event_loop()  # loop是一个消息循环对象
+loop.run_until_complete(init(loop))  # 在消息循环中执行协程
 loop.run_forever()
 
 
